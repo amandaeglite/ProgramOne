c9e0a8298bb558da6617b557d419cac39dca2bd8
ZibensMakvins <princecharmingthebeast@gmail.com>
2026-09-15 22:14:47 +0300

    Saule

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 ﻿print("LATVIEŠU DIEVĪBAS")
 print("=================")
 print()
-print("Latviešu mitoloģijas dievību enciklopēdija.")
+
+print("1. Saule")
+print()
+print("Saule ir viena no nozīmīgākajām dievišķajām būtnēm latviešu")
+print("mitoloģijā un folklorā. Viņa saistīta ar sauli, gaismu, dzīvību,")
+print("auglību un dabas ciklisko ritējumu.")
